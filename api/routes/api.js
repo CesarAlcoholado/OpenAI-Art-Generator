07de5d2cc5df6642bf88f042b80f1a1026ecfee1
Cesar <cesar.alcoholado@hotmail.com>
2024-06-14 10:01:12 -0300

    fixing cors issue

--- api/routes/api.js
+++ api/routes/api.js
@@ -3,6 +3,10 @@ const { generateImage } = require('../controllers/openAiControllers');
 
 const router = express.Router();
 
+router.get('/', (req,res)=> {
+  res.status(200).send("Hello World");
+})
+
 router.post('/images', generateImage);
 
 module.exports = router;

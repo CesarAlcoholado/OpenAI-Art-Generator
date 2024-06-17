979cb9b48719d032a48455709eb8dc2a7f60b14e
Cesar <cesar.alcoholado@hotmail.com>
2024-06-17 10:20:16 -0300

    deployment update

--- api/server.js
+++ api/server.js
@@ -2,16 +2,37 @@ require('dotenv').config();
 const express = require("express");
 const routes = require("./routes/api");
 const cors = require("cors");
+const bodyParser = require ("body-parser");
+const cookieParser = require("cookie-parser");
+const morgan = require("morgan");
 const app = express();
 
 const port = process.env.PORT;
 
 //parsing application/json
 app.use(express.json());
-
 //enable all request
 app.use(cors())
 
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(cookieParser());
+app.use(morgan("dev"));
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    // "https://dynamic-presentation-card-6bjh.vercel.app"
+  ); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
+
+
 app.use('/openai', routes);
 
-app.listen(port, ()=> console.log(`server is running at port http://localhost:${port}`));
+app.listen(port, ()=> console.log(`server is running at port ${port}`));
